fcf470f088fb4372d4a60847e123b3c91f87f6ed
Hans Phung <hans@dtn.com.vn>
2022-10-09 17:40:07 +0700

    webpack tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        port: 3000,
-        open: true,
-        compress: true,
-        historyApiFallback: true,
-    }
-}
+    port: 3000,
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
